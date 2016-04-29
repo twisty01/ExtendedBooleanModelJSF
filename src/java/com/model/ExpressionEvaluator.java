3509ec5fa7225e1782052c375506100801d8641f
martin <martin@martin-Extensa-2509>
2016-04-29 09:08:38 +0200

    Hraju si s parsrem (Expression)

--- src/java/com/model/ExpressionEvaluator.java
+++ src/java/com/model/ExpressionEvaluator.java
@@ -1,7 +1,24 @@
 package com.model;
 
+import java.util.List;
+
 
 public class ExpressionEvaluator  {
 
+    private String expression = "go and flower";        
+    private List<String> list;
+    
+    public List getList( String expression ) {
+       String[] terms;
+       
+       terms = expression.split(" ");
+       
+        for (String term : terms) {
+            
+        }
+       
+       return list;
+    }
+    
     
 }
